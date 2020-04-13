fd1b3090ce8d0e26dd822299a6578b2f7dcff3ad
adasumizox <darkdan099@gmail.com>
2020-04-13 21:23:57 +0200

    Bumped version and added typescript for future update

---
+++ src/js/models/city.ts
@@ -0,0 +1,4 @@
+class City {
+    city: String;
+    description: String;
+}
--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,20 @@ var path = require('path');
 module.exports = {
 	mode: 'development',
 	entry: './src/js/app.js',
+	resolve: {
+		extensions: [ '.tsx', '.ts', '.js' ],
+	},
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'bundle.js'
 	},
 	module: {
     	rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
 			{
 				test: /\.(png|jpe?g|gif)$/,
 				use: [
